b412a710bb2de1d4b9f698d62dff6d7934962895
rajsingh96199-hue <rajsingh96199.in>
2026-06-19 14:38:22 +0530

    fix render database connection

--- OneDrive/Desktop/prime-clinic/backend/db.js
+++ OneDrive/Desktop/prime-clinic/backend/db.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "prime_clinic",
-  password: "postgres123",
-  port: 5432,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
